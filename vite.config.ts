dd273938777e2113eeb9129b6a90e024a14a1a6d
dodumery <dodumery@gmail.com>
2025-12-31 16:50:38 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -21,3 +21,13 @@ export default defineConfig(({ mode }) => {
       }
     };
 });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    target: "esnext" // 
+  }
+})
